27407a2a62ffc9f0355afd3d65e68dffe2a1bd10
Rafael Lima <lima.rafa68@gmail.com>
2024-07-17 16:56:47 -0300

    secundo requisito

--- src/getMovieById.js
+++ src/getMovieById.js
@@ -1,7 +1,13 @@
 const movies = require("../data/movies");
 
 function getMovieById(id) {
-  
+  if (id === undefined || typeof id !== 'number') {
+    return undefined;
+  }
+
+  const movie = movies.find(movie => movie.id === id);
+
+  return movie;
 }
 
 module.exports = {
