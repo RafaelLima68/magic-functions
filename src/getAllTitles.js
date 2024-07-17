f0a15996df3e47780f971155c31419c7597b3ed6
Rafael Lima <lima.rafa68@gmail.com>
2024-07-17 16:55:36 -0300

    primeiro requisito

--- src/getAllTitles.js
+++ src/getAllTitles.js
@@ -1,7 +1,12 @@
 const movies = require("../data/movies");
 
 const getAllTitles = (movies) => {
- 
+ if (!movies || movies.length === 0) {
+  return [];
+ }
+
+  return movies.map(movies => movies.titulo)
+
 }
 
 module.exports = {
